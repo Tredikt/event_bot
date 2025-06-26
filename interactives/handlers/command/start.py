dee04f11dbfa4ab6d4d38ba5be3de895cc54e9a2
mastersurv <mastersurv.nikiforov@yandex.ru>
2025-06-26 23:17:01 +0300

    move text

--- interactives/handlers/command/start.py
+++ interactives/handlers/command/start.py
@@ -3,21 +3,12 @@ from aiogram.filters import Command
 from aiogram.types import Message
 
 from core.utils.enums import Variables
-from settings import config
+from core.utils.user_texts import START_TEXT
 
 
 router = Router(name="start_command")
 
-_START_TEXT = """
-Приветственное сообщение, план мероприятия, обзор по функиционалу бота, повествование, 
-что будет в этом боте сегодня, рассказ про геймификацию в боте, что за активность начисляются 
-баллы, по которым разыграется приз в конце
-
-Контакты организаторов мероприятия, по всем вопросам обращайтесь сюда
-Ожидайте начала первой активности (первого выступления спикера)
-"""
-
 
 @router.message(Command(commands=["start"]))
 async def start_command_handler(message: Message, variables: Variables):
-    await message.answer(text=_START_TEXT)
+    await message.answer(text=START_TEXT)
