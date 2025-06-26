ad915f8616adc0c15a14efef4b56aef81c50afca
v01cee <maksimagrig2@gmail.com>
2025-06-26 20:41:44 +0300

    fix bugs

--- interactives/handlers/callback/__init__.py
+++ interactives/handlers/callback/__init__.py
@@ -1,11 +1,14 @@
-from interactives.handlers.callback.gavrikov import router as gavrikov_router
-from interactives.handlers.callback.belozyortseva import router as belozyortseva_router
+from interactives.handlers.callback.gavrikov import router as gavrikov_callback_router
+from interactives.handlers.callback.belozyortseva import router as belozyortseva_callback_router
 from interactives.handlers.callback.mendubaev import router as mendubaev_router
 from interactives.handlers.callback.speaker_sadriev import router as sadriev_router
-from interactives.handlers.callback.speaker_gilmanova import router as gilmanova_router
-from interactives.handlers.callback.speaker_horoshutina import router as horoshine_router
+from interactives.handlers.callback.speaker_gilmanova import router as gilmanova_callback_router
+from interactives.handlers.callback.speaker_horoshutina import router as horoshine_callback_router
+from interactives.handlers.callback.zabegayev import router as zabegayev_callback_router
+from interactives.handlers.callback.zargaryan import router as zargaryan_callback_router
 
 from interactives.handlers.callback.ending import router as ending
+from interactives.handlers.state.zargaryan import router as zargaryan_state_router
 
 
 callback_routers = [
@@ -15,5 +18,8 @@ callback_routers = [
     sadriev_router,
     gilmanova_router,
     horoshine_router,
-    ending
+    ending,
+    zabegayev_callback_router,
+    zargaryan_callback_router,
+    zargaryan_state_router
 ]
