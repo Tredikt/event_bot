6e47d340a7cdfc148d49230ffa7efaf257703940
mastersurv <mastersurv.nikiforov@yandex.ru>
2025-07-07 21:41:44 +0300

    change db

--- settings.py
+++ settings.py
@@ -31,5 +31,5 @@ config = Config()
 DB_URL: str = (
      "postgresql+asyncpg://"
      f"{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_HOST}:{config.DB_PORT}/{config.DB_NAME}"
-     # "sqlite+aiosqlite:///event_bot.db"
+    # "sqlite+aiosqlite:///database.db"
 )
