1338020f203c25ae8620a0750e441317624fe50c
mastersurv <mastersurv.nikiforov@yandex.ru>
2025-07-08 20:01:35 +0300

    fix conflicts

--- interactives/handlers/callback/gavrikov.py
+++ interactives/handlers/callback/gavrikov.py
@@ -22,24 +22,12 @@ async def gavrikov_callback_handler(call: CallbackQuery, variables: Variables):
     await variables.bot.send_chat_action(chat_id=user_id, action=ChatAction.TYPING)
     await asyncio.sleep(1)
 
-<<<<<<< HEAD
     photo = "AgACAgIAAxkBAAIP3WhqVBzKLgJUGDTcMMC5RzughbDoAALe8jEbF0NZS7gvl5T9lwNnAQADAgADeQADNgQ"
     await call.message.answer_photo(
         photo=photo, 
         caption="📍 <b>Вопрос для разогрева\n\nКто понимает, откуда берутся все эти цифры и как формируются начисления?</b>",
         reply_markup=await variables.keyboards.menu.gavrikov_start()
     )
-=======
-    photo = "AgACAgIAAxkBAAIL_GhoYIq9tKqZCIaXcLsX7faQR_I2AAKZ9jEb7Eg5S0j6L_xGf7ilAQADAgADeQADNgQ"
-    await call.message.answer_photo(photo=photo)
-
-    await variables.bot.send_chat_action(chat_id=user_id, action=ChatAction.TYPING)
-    await asyncio.sleep(1)
-
-    text = "Выбери свой вариант"
-    keyboard = await variables.keyboards.menu.gavrikov_start()
-    await call.message.answer(text=text, reply_markup=keyboard)
->>>>>>> 4c4a923 (fix_bugs)
 
 
 @router.callback_query(F.data.startswith("gavrikov_pictures"))
